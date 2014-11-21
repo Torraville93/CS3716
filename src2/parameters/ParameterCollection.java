f800218d5166eaf444a411f121fcfb66809a89fe
uoshy <xuoshy@gmail.com>
2014-11-21 08:43:19 -0330

    Create ParameterCollection.java

---
+++ src2/parameters/ParameterCollection.java
@@ -0,0 +1,25 @@
+package parameters;
+
+import java.util.ArrayList;
+
+/**
+ * Singleton collection of Parameter objects
+ * @param <GroupParameter>
+ */
+public class ParameterCollection<GroupParameter> {
+
+	private static ArrayList singleton = new ArrayList();
+	   
+	   /** A private Constructor prevents any other 
+	    *  class from instantiating.
+	    */
+	   private ParameterCollection() { }
+	   
+	   /**
+	    * Static 'instance' method
+	    * @return singletion collection
+	    */
+	   public static ArrayList getInstance() {
+		   return singleton;
+	   }
+}

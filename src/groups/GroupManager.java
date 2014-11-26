ae1fed9505620b9e479617ebfc9c2072f5ebab00
uoshy <xuoshy@gmail.com>
2014-11-26 10:30:01 -0330

    Update GroupManager.java

--- src/groups/GroupManager.java
+++ src/groups/GroupManager.java
@@ -11,7 +11,7 @@ import users.Student;
 public class GroupManager {
 	
 	private GeneratorStrategy strategy;
-	public static int groupSize;
+	public static int groupSize = 3; //Default group size
 	private ArrayList<Group> groups;
 	
 	public GroupManager() {}
@@ -49,58 +49,4 @@ public class GroupManager {
 	public void addGroup(Group aGroup) {
 		groups.add(aGroup);
 	}
-	
-	/*
-	import Retrevial.StudentsFromFile;
-    import SystemUI.GroupPreferenceManager_UI;
- 
-	 
-	private Generator.GeneratorStrategy strategy;
-	GroupPreferenceManager_UI groupUI = new GroupPreferenceManager_UI();	
-	private ArrayList<Student> studentList;
-	private int groupSize;
-	
-	public GroupManager(Generator.GeneratorStrategy strategy) {
-		this.strategy = strategy;
-	}
-
-	public void setGroupSize(int i){
-		groupSize = i;
-	}
-	
-	public int getGroupSize(){
-		return groupSize;
-	}
-	
-	public ArrayList<Group> createGroups() throws IOException{
-	//	StudentsFromFile sff = new StudentsFromFile();
-	//	studentList = sff.getStudents();	
-		
-		ArrayList<Student> studentList = new ArrayList<Student>();
-		
-		Student s1 = new Student("Marc", 1);
-		Student s2 = new Student("John", 2);
-		Student s3 = new Student("Jean", 3);
-		Student s4 = new Student("Solid Snake", 4);
-		Student s5 = new Student("Woody Harrelson", 5);
-		Student s6 = new Student("Mr. T", 6);
-		Student s7 = new Student("Mrs. T", 7);
-		Student s8 = new Student("Chair", 8);
-		Student s9 = new Student("Desk",9);
-		Student s10 = new Student("Liquid Snake", 10);
-		
-		studentList.add(s1);
-		studentList.add(s2);
-		studentList.add(s3);
-		studentList.add(s4);
-		studentList.add(s5);
-		studentList.add(s6);
-		studentList.add(s7);
-	    studentList.add(s8);
-		studentList.add(s9);
-		studentList.add(s10);
-		
-		return this.strategy.generateGroups(studentList, 3);
-	}*/
-
 }

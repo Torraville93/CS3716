c71b3e993bbdbd914737bccfd20cf93bff46f7e9
uoshy <xuoshy@gmail.com>
2014-11-26 11:21:52 -0330

    Update SystemUI.java
    Added fancy ol' combo box (drop-down menu).
    Manually option currently does nothing.

--- src/systemUI/SystemUI.java
+++ src/systemUI/SystemUI.java
@@ -14,13 +14,16 @@ import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import retrieval.*;
 import users.*;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -39,6 +42,7 @@ public class SystemUI extends JFrame {
 	private JTextField txtGroupSize;
 	private JTextField txtDeadline;
 	private JTextArea txtDescription;
+	private JComboBox stuRetrievalBox;
 	private GroupSystem system;
 	
 	/**
@@ -47,7 +51,7 @@ public class SystemUI extends JFrame {
 	public SystemUI() {
 		super("Group Generator System");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 407, 368);
+		setBounds(100, 100, 407, 398);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -78,60 +82,49 @@ public class SystemUI extends JFrame {
 		txtDeadline.setBounds(271, 90, 103, 27);
 		contentPane.add(txtDeadline);
 		
+		JLabel lblEnterRetrieval = new JLabel("Enter the desired student-retrieval:");
+		lblEnterRetrieval.setBounds(12, 133, 250, 15);
+		contentPane.add(lblEnterRetrieval);
+		String[] retrievalTypes = { "From file", "Manually"};
+		stuRetrievalBox = new JComboBox(retrievalTypes);
+		stuRetrievalBox.setBounds(271, 129, 103, 27);
+		contentPane.add(stuRetrievalBox);
+		
 		JLabel lblEnterDescription = new JLabel("Enter project description:");
-		lblEnterDescription.setBounds(12, 133, 250, 15);
+		lblEnterDescription.setBounds(12, 178, 250, 15);
 		contentPane.add(lblEnterDescription);
 		txtDescription = new JTextArea();
 		JScrollPane sp = new JScrollPane(txtDescription);
-		sp.setBounds(12, 152, 362, 127);
+		sp.setBounds(12, 202, 362, 127);
 		add(sp);
 		
 		JButton btnSubmitButton = new JButton("Submit");
 		btnSubmitButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent event) {
-				int tmp = 0;
+				int tmp = system.getGroupManager().getGroupSize(); //Set to default size
 				try {
 					tmp = Integer.parseInt(txtGroupSize.getText());
-				}
-				catch(NumberFormatException e){
-					e.printStackTrace();
-				}
+				} catch(NumberFormatException e){ e.printStackTrace(); }
 				system.getGroupManager().setGroupSize(tmp);
+				System.out.println("Group Size: "+system.getGroupManager().getGroupSize());
 				system.setCourseName(txtCourseName.getText());
+				System.out.println("Course Name: "+system.getCourseName());
 				system.setDeadline(txtDeadline.getText());
-				system.setDescription(txtDescription.getText());
-				
-				ArrayList<Group> tmpList = 
-						system.getGroupManager().generateGroups(new SimpleStrat());
-				for(Group grp: tmpList) {
-					for (Student stu: grp.getStudents()) {
-						String str = stu.getName() +" "+ stu.getID() +" ";
-						System.out.println(str);
-					}
+				System.out.println("Deadline: "+system.getDeadline());
+				if (stuRetrievalBox.getSelectedIndex() == 0) {
+					system.setRetrievalMethod(new StudentsFromFile());
+					try {
+						system.students = system.getRetrievalMethod().getStudents();
+					} catch (IOException e) { e.printStackTrace(); }
 				}
-				//Output to file for now
-				/*ArrayList<Group> tmpList = 
-						system.getGroupManager().generateGroups(new SimpleStrat());
-				FileWriter writer;
-				try {
-					writer = new FileWriter("output.txt");
-					for(Group grp: tmpList) {
-						for (Student stu: grp.getStudents()) {
-							String str = stu.getName() +" "+ stu.getID() +" ";
-							System.out.println(str);
-							writer.write(str); 
-						}
-					}
-					
-					writer.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} */
-				
+				//else other method of getting students
+				System.out.println("Added students: "+system.students.size());
+				system.setDescription(txtDescription.getText());
+				System.out.println("Description: "+system.getDescription());
 			}
 		});
-		btnSubmitButton.setBounds(271, 300, 100, 20);
+		btnSubmitButton.setBounds(271, 335, 100, 20);
 		contentPane.add(btnSubmitButton);
 	}
 	
@@ -149,76 +142,3 @@ public class SystemUI extends JFrame {
 		});
 	}
 }
-
-
-
-
-
-/*
-public class GroupPreferenceManager_UI extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField txtCourseNumber;
-	private JTextField txtGroupSize;
-	public String courseNum;
-	public int groupSize;
-
-	 // Launch the application.
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GroupPreferenceManager_UI frame = new GroupPreferenceManager_UI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	 // Create the frame.
-	public GroupPreferenceManager_UI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 407, 168);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton btnSubmitButton = new JButton("Submit");
-		btnSubmitButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				groupSize = Integer.parseInt(txtGroupSize.getText());
-				courseNum = txtCourseNumber.getText();
-				GroupSystem sys = new GroupSystem();
-				sys.setGroupSize(groupSize);
-			}
-		});
-		btnSubmitButton.setBounds(271, 88, 106, 40);
-		contentPane.add(btnSubmitButton);
-		
-		JLabel lblEnterCourse = new JLabel("Enter the desired course number:");
-		lblEnterCourse.setBounds(12, 16, 250, 15);
-		contentPane.add(lblEnterCourse);
-		
-		txtCourseNumber = new JTextField();
-		txtCourseNumber.setBounds(271, 10, 103, 27);
-		contentPane.add(txtCourseNumber);
-		txtCourseNumber.setColumns(10);
-		
-		txtGroupSize = new JTextField();
-		txtGroupSize.setColumns(10);
-		txtGroupSize.setBounds(271, 49, 103, 27);
-		contentPane.add(txtGroupSize);
-		
-		JLabel lblEnterTheDesired = new JLabel("Enter the desired group size:");
-		lblEnterTheDesired.setBounds(12, 55, 250, 15);
-		contentPane.add(lblEnterTheDesired);
-	}
-	
-	public int getSizeOfGroup() {
-		return groupSize;
-	}
-}*/

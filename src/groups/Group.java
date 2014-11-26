8c83cc88152003615b0bb0b4d41ff8deb76d82f4
uoshy <xuoshy@gmail.com>
2014-11-26 10:29:43 -0330

    Update Group.java

--- src/groups/Group.java
+++ src/groups/Group.java
@@ -1,16 +1,17 @@
 package groups;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import users.Student;
 
 public class Group {
 	
-	private ArrayList<Student> stuList;
+	private List<Student> stuList;
 	
 	public Group() { stuList = new ArrayList<Student>(); }
-	public Group(ArrayList<Student> students) {
-		stuList = students;
+	public Group(List<Student> tmpGrp) {
+		stuList = tmpGrp;
 	}
 	
 	public void addStudent(Student student){
@@ -21,26 +22,7 @@ public class Group {
     	return stuList.remove(student);
     }
 	
-    public ArrayList<Student> getStudents() {
+    public List<Student> getStudents() {
     	return stuList;
     }
-	
-	
-	
-	/*
-	ArrayList<Student> studentGroup = new ArrayList<Student>();
-	
-	public void addStudent(Student student){
-		studentGroup.add(student);
-	}
-
-    public boolean removeStudent(Student student){
-    	return studentGroup.remove(student);
-    }
-
-    public void getStudentNames() {
-    	for (int i = 0 ; i < studentGroup.size() ; i++) {
-    		System.out.println(studentGroup.get(i).getName());
-    	}
-    }*/
 }

55bd358b5eba3e7a1b26098fc577505a842a9e99
uoshy <xuoshy@gmail.com>
2014-11-26 10:33:39 -0330

    Update and rename test.java to GeneratorTest.java

---
+++ src/GeneratorTest.java
@@ -0,0 +1,38 @@
+import generator.*;
+import groupSystem.GroupSystem;
+import groups.Group;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import retrieval.*;
+import systemUI.*;
+import users.*;
+
+
+public class GeneratorTest {
+	
+	public static void main(String[] args) throws IOException {
+		
+		GroupSystem system = new GroupSystem();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please input group size(int): ");
+		int size = in.nextInt();
+		system.getGroupManager().setGroupSize(size);
+		
+		RetrieveStudents rs = new StudentsFromFile();
+		GroupSystem.students = rs.getStudents();
+
+		GeneratorStrategy gs = new SimpleStrat();
+		ArrayList<Group> aa = gs.generateGroups();
+		int grpNum = 1;
+		System.out.println("\nGenerating groups using simple strategy: ");
+		for (Group grp: aa) {
+			System.out.println("--Group "+grpNum); grpNum++; 
+			for (Student stu: grp.getStudents()) {
+				System.out.println("\t"+stu.getName());
+			}
+		}
+	}
+}

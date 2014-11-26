bbee84464b9f72d01640f9d6b164c982603bd5e0
uoshy <xuoshy@gmail.com>
2014-11-26 10:29:13 -0330

    Update GroupSystem.java

--- src/groupSystem/GroupSystem.java
+++ src/groupSystem/GroupSystem.java
@@ -28,7 +28,10 @@ public class GroupSystem {
 	
 	public void setDeadline(String date){
 		deadline = date;
-		//System.out.println(deadline);
+	}
+	
+	public String getDeadline() {
+		return deadline;
 	}
 	
 	public void addStudent(Student student){
@@ -60,7 +63,6 @@ public class GroupSystem {
 
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
-		//System.out.println(this.courseName);
 	}
 
 	public GroupManager getGroupManager() {
@@ -77,77 +79,5 @@ public class GroupSystem {
 
 	public void setDescription(String description) {
 		this.description = description;
-		//System.out.println(this.description);
 	}
-	
-    
-    
-    
-    /*package SystemFiles;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-
-import Groups.Group;
-import Groups.GroupManager;
-import Parameters.*;
-import Users.*;
-
-
-public class GroupSystem {
-	
-	ArrayList<Student> students;
-	private String deadline;
-	private Instructor instructor;
-	private GroupManager groupManager;
-	private ArrayList<GroupParameter> params;
-	
-	public GroupSystem() {
-		students = new ArrayList<Student>();
-		instructor = new Instructor();
-		groupManager = new GroupManager(new Generator.SimpleGenerate());
-	}
-	
-	public void setDeadline(String date){
-		deadline = date;
-	}
-	
-	
-    public Student getStudent(Number number) {
-    	for (Student stu : students) {
-    		if (stu.getNumber() == number) return stu;
-    	}
-    	return null;
-    }
-    
-    public void addInstructor(Instructor instructor) {
-    	this.instructor = instructor;
-    }
-    
-    public ParameterSpec getParameters() {
-    	return instructor.getParameters();
-    }
-    
-	public void setGroupSize(int groupSize){
-		groupManager.setGroupSize(groupSize);
-	}
-	
-	public int getGroupSize(){
-		return groupManager.getGroupSize();
-	}
-	
-	public static void main(String[] args) throws IOException {
-		GroupManager gm;
-		gm = new GroupManager(new Generator.SimpleGenerate());
-		ArrayList<Group> groupList = gm.createGroups();
-		
-		for (int i = 0 ; i < groupList.size(); i++) {
-			groupList.get(i).getStudentNames();
-			System.out.println("");
-		}
-	}
-	
-	
-}*/
 }

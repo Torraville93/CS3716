d2f4b4ced1a2d250af5f014db4c36027cee2be28
uoshy <xuoshy@gmail.com>
2014-11-27 18:41:24 -0330

    Update GroupParameter.java

--- src/parameters/GroupParameter.java
+++ src/parameters/GroupParameter.java
@@ -15,12 +15,4 @@ public class GroupParameter {
 	public ParameterSpec getSpec() {
 		return spec;
 	}
-	
-	public void addParameter(Param propertyName, String property) {
-		spec.getProperties().put(propertyName, property);
-	}
-	
-	public String removeParameter(Param propertyName) {
-		return spec.getProperties().remove(propertyName);
-	}
 }

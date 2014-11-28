26e89bcc8caa2786e0691a46344a70e73bedebec
uoshy <xuoshy@gmail.com>
2014-11-28 06:45:05 -0330

    Update ParameterStrat.java

--- src/generator/ParameterStrat.java
+++ src/generator/ParameterStrat.java
@@ -10,19 +10,18 @@ import java.util.List;
 import parameters.GroupParameter;
 import users.Student;
 
-//Currently identical to SimpleStrat just so the UI doesn't freak out when we select this strategy.
-//Also renamed it to ParameterStrat instead of DecentStrat
 public class ParameterStrat implements GeneratorStrategy {
-	
 
-	//@Override
+	int numStudents;
+	int grpSize;
+	ArrayList<Group> tmp;
+	
+	@Override
 	public ArrayList<Group> generateGroups() {
 		ArrayList<Group> tmp = new ArrayList<Group>();
 		int numStudents = GroupSystem.students.size();
 		int grpSize = GroupManager.groupSize;
-		
-		List<Student> testGroup = GroupSystem.students;
-	
+		sortByGPA(GroupSystem.students);
 		
 		for (int i = 0; i < numStudents; i += grpSize) {
 			int end = Math.min(i + grpSize, numStudents);
@@ -30,6 +29,61 @@ public class ParameterStrat implements GeneratorStrategy {
 			tmp.add(new Group(tmpGrp));
 		}
 		return tmp;
-	}
+	}	
 
+	//Unused. Here JIC
+	public ArrayList<Group> groupByGPA(List<Student> grps) {
+		for (int i = 0; i < grps.size(); i += grpSize) {
+			int end = Math.min(i + grpSize, grps.size());
+			List<Student> tmpGrp = GroupSystem.students.subList(i, end);
+			tmp.add(new Group(tmpGrp));
+		}
+		return tmp;
+	}
+	
+	//Unused. Here JIC
+	public List<Group> byExperience(List<Group> grps) {
+		return null;
+	}
+	
+	//Unused. Here JIC
+	public double getMaxGPA(List<Student> stus) {
+		double max=0; int count=0;
+		for (int i = 0; i < stus.size()-1; i ++) {
+			if (Double.parseDouble(stus.get(i).getParameters().get(count).toString().split(",")[2]) >
+			Double.parseDouble(stus.get(i+1).getParameters().get(count).toString().split(",")[2])) {
+				max = Double.parseDouble(stus.get(i).getParameters().get(count).toString().split(",")[2]);
+			} else
+				max = Double.parseDouble(stus.get(i+1).getParameters().get(count).toString().split(",")[2]);
+		}
+		return max;
+	}
+	
+	//Unused. Here JIC
+	public double getMinGPA(List<Student> stus) {
+		double min=0; int count=0;
+		for (int i = 0; i < stus.size()-1; i ++) {
+			if (Double.parseDouble(stus.get(i).getParameters().get(count).toString().split(",")[2]) <
+			Double.parseDouble(stus.get(i+1).getParameters().get(count).toString().split(",")[2])) {
+				min = Double.parseDouble(stus.get(i).getParameters().get(count).toString().split(",")[2]);
+			} else
+				min = Double.parseDouble(stus.get(i+1).getParameters().get(count).toString().split(",")[2]);
+		}
+		return min;
+	}
+	
+	public List<Student> sortByGPA(List<Student> list) {
+        for (int i = 1; i < list.size(); i++) {
+            Student next = list.get(i);
+            // find the insertion location while moving all larger element up
+            int j = i;
+            while (j > 0 && Double.parseDouble(list.get(j-1).getParameters().get(0).toString().split(",")[2]) > Double.parseDouble(next.getParameters().get(0).toString().split(",")[2])) {
+            	list.set(j, list.get(j-1));
+            	j--;
+            }
+            // insert the element
+            list.set(j, next);
+        }
+        return list;
+    }
 }

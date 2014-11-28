fe2618b17cbcd728f3329d33225c86952763739c
uoshy <xuoshy@gmail.com>
2014-11-28 06:46:48 -0330

    Update ParameterSpec.java

--- src/parameters/ParameterSpec.java
+++ src/parameters/ParameterSpec.java
@@ -1,14 +1,18 @@
 package parameters;
 import java.util.EnumMap;
+import java.util.Map.Entry;
 
 public class ParameterSpec {
 	
 	private EnumMap<Param,String> properties = null;
 	
-	public ParameterSpec() { 
-		properties = new EnumMap<Param, String>(Param.class); 
+	public ParameterSpec() { properties = new EnumMap<Param, String>(Param.class); }
+	//Copy constructor for "deep cloning"
+	public ParameterSpec(ParameterSpec aSpec) {
+		for (Entry<Param, String> e: aSpec.getProperties().entrySet()) {
+			properties.put(e.getKey(),e.getValue());
 		}
-	
+	}
 	public ParameterSpec(EnumMap<Param,String> properties) {
 		if (properties == null) {
 			this.properties = new EnumMap<Param, String>(Param.class);;
@@ -37,8 +41,8 @@ public class ParameterSpec {
 		return properties;
 	}
 	
-	public String toString(){
-		return properties.toString();
-		
+	public String toString() {
+		return properties.get(Param.NAME)+", "+properties.get(Param.QUERY)+
+				", "+properties.get(Param.RESPONSE);
 	}
 }

3da5bfdffae5dbb15013090d5ebe5b0826b56954
mcroke77 <mcroke77@gmail.com>
2014-12-01 23:52:40 -0330

    Update Student.java

--- src/users/Student.java
+++ src/users/Student.java
@@ -33,29 +33,4 @@ public class Student extends User {
 	public ParameterSpec getParameters() {
 	   	return super.getParameters();
 	}
-	
-	
-	
-
-	/*private String name;
-	private Number number;
-	
-	public Student() {}
-
-    public void setName(String name){
-    	this.name = name;
-    }
-
-    public String getName(){
-    	return name;
-    }
-
-    public void setNumber(Number number){
-    	this.number = number;
-    }
-
-    public Number getNumber(){
-    	return number;
-    }*/
-
 }

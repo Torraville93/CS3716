350fc74d66ca9335b201c4c36a805ad8e8b51ecf
uoshy <xuoshy@gmail.com>
2014-12-03 00:46:20 -0330

    Update RetrieveStudents.java

--- src/retrieval/RetrieveStudents.java
+++ src/retrieval/RetrieveStudents.java
@@ -1,15 +1,22 @@
 package retrieval;
-/*
- * An interface to allow the system to retrieve students
- */
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 import users.Student;
 
-
+/**
+ * RetrieveStudents interface to allow the use of varying ways to add 
+ * students to the system. 
+ * Follows the strategy design pattern.
+ */
 public interface RetrieveStudents {
 	
+	/**
+	 * Get list of Student's based on concrete method.
+	 * @return - list of Student's
+	 * @throws IOException - when getting from file.
+	 */
 	public ArrayList<Student> getStudents() throws IOException ;
 
 }

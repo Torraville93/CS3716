dc7b448b9b0b92c19779e27d4908713e0e57bced
uoshy <xuoshy@gmail.com>
2014-12-03 00:44:00 -0330

    Update SimpleStrat.java

--- src/generator/SimpleStrat.java
+++ src/generator/SimpleStrat.java
@@ -3,23 +3,26 @@ package generator;
 import groupSystem.*;
 import groups.*;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import retrieval.RetrieveStudents;
-import retrieval.StudentsFromFile;
 import users.Student;
-/*
-*  SimpleStrat generates groups from given size by taking each student 
-*  from being of list to end. No addition information.
-*/
+
+/**
+ * Group generating strategy that assigns students to groups based 
+ * on group size. Iterates through list of students creating somewhat 
+ * unfair groups. Remaining students are added to an extra group if 
+ * number of students isn't divisible by group size.
+ */
 public class SimpleStrat implements GeneratorStrategy {
 
-	//@Override
+	/**
+	 * Generates groups of students of given size.
+	 * @return - list of student Group's
+	 */
+	@Override
 	public ArrayList<Group> generateGroups() {
 		ArrayList<Group> tmp = new ArrayList<Group>();
-		Group group = new Group();
 		int numStudents = GroupSystem.students.size();
 		int grpSize = GroupManager.groupSize;
 		

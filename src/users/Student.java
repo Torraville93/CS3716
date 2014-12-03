efe62f047b151505c002bd568034b98b61c10528
uoshy <xuoshy@gmail.com>
2014-12-03 00:50:41 -0330

    Update Student.java

--- src/users/Student.java
+++ src/users/Student.java
@@ -1,36 +1,74 @@
 package users;
 
-import parameters.Param;
-import parameters.ParameterSpec;
+import java.util.List;
 
-public class Student extends User {
+import parameters.GroupParameter;
+
+/**
+ * Concrete User student, handles student information like 
+ * name and ID, and list of GroupParameters with responses.
+ */
+public class Student implements User {
 		
+	private String name;
+	private Number numID;
+	private List<GroupParameter> parameters;
+	
+	/**
+	 * Constructors. Initialises list of info.
+	 */
 	public Student() {}
 	public Student(String name, Number numID) {
-		super(name,numID);
+		this.name = name; 
+		this.numID = numID;
 	}
-		
+
+	/**
+	 * Set student name.
+	 * @param name - instructor name.
+	 */
 	public void setName(String name){
-	   	super.setName(name);
+		this.name = name;
 	}
-
+	
+	/**
+	 * Get student name.
+	 * @return - a name.
+	 */
 	public String getName(){
-	   	return super.getName();
-	}
-
+    	return name;
+    }
+	    
+	/**
+	 * Set student ID.
+	 * @param numID - an ID
+	 */
 	public void setID(Number numID){
-	   	super.setID(numID);
+    	this.numID = numID;
+    }
+	    
+	/**
+	 * Get student ID.
+	 * @return - an ID
+	 */
+	public Number getID(){
+    	return numID;
+    }
+
+	/**
+	 * Get list of GroupParameter's
+	 * @return - list of GroupParameter's.
+	 */
+	public List<GroupParameter> getParameters() {
+		return parameters;
+	}
+	
+	/**
+	 * Set list of GroupParameter's
+	 * @param - list of GroupParameter's.
+	 */
+	public void setParameters(List<GroupParameter> paramList) {
+		parameters = paramList;
 	}
 
-	public Number getID(){
-	   	return super.getID();
-	}
-	    
-	public void addParameter(Param propertyName, String property) {
-	   	super.addParameter(propertyName, property);
-	}
-	    
-	public ParameterSpec getParameters() {
-	   	return super.getParameters();
-	}
 }

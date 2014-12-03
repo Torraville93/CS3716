ab7053c10959bcbf177907ec46e3106a7036b713
uoshy <xuoshy@gmail.com>
2014-12-03 00:50:25 -0330

    Update Instructor.java

--- src/users/Instructor.java
+++ src/users/Instructor.java
@@ -1,35 +1,83 @@
 package users;
 
+import java.util.List;
+
 import parameters.*;
 
-public class Instructor extends User {
+/**
+ * Concrete User instructor, handles instructor information like 
+ * name and ID, and list of instructor-added GroupParameter's.
+ */
+public class Instructor implements User {
 	
-	public Instructor() {}
+	private String name;
+	private Number numID=0;//Default Instructor ID (using for now)
+	private List<GroupParameter> parameters;
+		
+	/**
+	 * Constructors. Initialises info and list of parameters.
+	 */
+	public Instructor() { parameters = ParameterCollection.getInstance(); }
 	public Instructor(String name, Number numID) {
-		super(name,numID);
+		this.name = name; 
+		this.numID = numID;
+		parameters = ParameterCollection.getInstance();
 	}
 	
-    public void setName(String name){
-    	super.setName(name);
+	/**
+	 * Set instructor name.
+	 * @param name - instructor name.
+	 */
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	/**
+	 * Get instructor name.
+	 * @return - a name.
+	 */
+	public String getName(){
+    	return name;
+    }
+	    
+	/**
+	 * Set instructor ID.
+	 * @param numID - an ID
+	 */
+	public void setID(Number numID){
+    	this.numID = numID;
+    }
+	    
+	/**
+	 * Get instructor ID.
+	 * @return - an ID
+	 */
+	public Number getID(){
+    	return numID;
     }
 
-    public String getName(){
-    	return super.getName();
-    }
+	/**
+	 * Add a GroupParameter to list of parameters.
+	 * @param grpParam - a GroupParameter.
+	 */
+	public void addParameter(GroupParameter grpParam) {
+		parameters.add(grpParam);
+	}
+	
+	/**
+	 * Remove a GroupParameter from list of parameters.
+	 * @param grpParam - a GroupParameter.
+	 * @return - boolean, whether it succeeds or not.
+	 */
+	public boolean removeParameter(GroupParameter grpParam) {
+		return parameters.remove(grpParam);
+	}
 
-    public void setID(Number numID){
-    	super.setID(numID);
-    }
-
-    public Number getID(){
-    	return super.getID();
-    }
-    
-    public void addParameter(Param propertyName, String property) {
-    	super.addParameter(propertyName, property);
-    }
-    
-    public ParameterSpec getParameters() {
-    	return super.getParameters();
-    }
+	/**
+	 * Get list of GroupParameter's
+	 * @return - list of GroupParameter's.
+	 */
+	public List<GroupParameter> getParameters() {
+		return parameters;
+	}
 }
